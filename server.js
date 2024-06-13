7dfb5f6908ccbc04cb137261ca25477323d7f203
Daz3Crud <lukydaze@gmail.com>
2024-06-13 00:28:28 -0400

    add hash around as compare function

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const bcrypt = require('bcrypt');
+
+const app = express();
 
 fccTesting(app);
 const saltRounds = 12;
@@ -16,46 +16,38 @@ app.get('/', (req, res) => {
 });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(hash + " <= this is the bcrypt hash");
 
+    // Compare the password after it has been hashed
+    bcrypt.compare(myPlaintextPassword, hash, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(result, " <= result here"); // True if passwords match
 
-
+        // Change the password to a different one to see false result
+        bcrypt.compare(someOtherPlaintextPassword, hash, (err, result) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(result, " <= result here with different password"); // False if passwords do not match
+        });
+    });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+// Synchronous code can be placed here
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+    console.log("Listening on port:", PORT);
 });
